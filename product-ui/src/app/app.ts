d2ee75bb471f3a34aaedb8e0ea4af5a8a725940f
jronceros80 <jronceros80@gmail.com>
2025-06-28 16:44:07 +0200

    chore: update angular.json and tsconfig.json for style changes and TypeScript configuration improvements

--- product-ui/src/app/app.ts
+++ product-ui/src/app/app.ts
@@ -3,10 +3,13 @@ import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
   imports: [RouterOutlet],
-  template: '<router-outlet></router-outlet>',
-  styleUrl: './app.css'
+  template: `
+    <router-outlet></router-outlet>
+  `,
+  styles: []
 })
 export class App {
-  protected title = 'Product Management';
+  title = 'product-ui';
 }
